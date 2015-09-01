f76ab9d9273e3533c7eb4ef54cdcb69ad76abad2
elivoa <elivoa@gmail.com>
2015-09-01 23:28:15 +0800

    revel-replace.go

--- utils/revex/revel-replace.go
+++ utils/revex/revel-replace.go
@@ -1,9 +1,12 @@
 package revex
 
 import (
+	"bytes"
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 )
 
 // Reve* Configs
@@ -16,3 +19,41 @@ var (
 	WARN  = log.New(ioutil.Discard, "WARN  ", log.Ldate|log.Ltime|log.Lshortfile)
 	ERROR = log.New(os.Stderr, "ERROR ", log.Ldate|log.Ltime|log.Lshortfile)
 )
+
+type ExecutableTemplate interface {
+	Execute(io.Writer, interface{}) error
+}
+
+// Execute a template and returns the result as a string.
+func ExecuteTemplate(tmpl ExecutableTemplate, data interface{}) string {
+	var b bytes.Buffer
+	tmpl.Execute(&b, data)
+	return b.String()
+}
+
+// Reads the lines of the given file.  Panics in the case of error.
+func MustReadLines(filename string) []string {
+	r, err := ReadLines(filename)
+	if err != nil {
+		panic(err)
+	}
+	return r
+}
+
+// Reads the lines of the given file.  Panics in the case of error.
+func ReadLines(filename string) ([]string, error) {
+	bytes, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return strings.Split(string(bytes), "\n"), nil
+}
+
+func ContainsString(list []string, target string) bool {
+	for _, el := range list {
+		if el == target {
+			return true
+		}
+	}
+	return false
+}
